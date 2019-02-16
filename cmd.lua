285b12bfa7baa083aedda76bd98f47daa845eeaa
jryzkns <xza194@sfu.ca>
2019-02-16 08:57:08 -0800

    base game completed

--- cmd.lua
+++ cmd.lua
@@ -11,6 +11,7 @@ local cmd = {}
 
 cmd.prompt = "Chase@HOPE-SS> " -- this will dynamically change with the narrative
 
+cmd.power = 100
 cmd.command = ""
 cmd.history = {}
 cmd.commands_issued = 0
@@ -21,10 +22,12 @@ cmd.font = love.graphics.setNewFont("BebasNeue-Regular.ttf",30)
 cmd.failedcmd = false
 cmd.idle = 0
 cmd.response = ""
+                
+function cmd:textinput(char) cmd.command = cmd.command .. char end
+cmd.xdim,cmd.ydim = 0,0
+function cmd:getGameState(game) if game.xdim and game.ydim then cmd.xdim, cmd.ydim = game.xdim, game.ydim end end
 
-function cmd:update(dt,frames)
-        cmd.idle = cmd.idle + 1
-end
+function cmd:update(dt,frames) cmd.idle = cmd.idle + 1 end
 
 function cmd:keypressed(key,scancode,isrepeat)
         cmd.idle = 0
@@ -48,22 +51,50 @@ function cmd:keypressed(key,scancode,isrepeat)
         end
 end
 
-function cmd:textinput(char) cmd.command = cmd.command .. char end
-
-cmd.xdim,cmd.ydim = 0,0
-function cmd:getGameState(game) if game.xdim and game.ydim then cmd.xdim, cmd.ydim = game.xdim, game.ydim end end
-
-
 cmd.commandtable = {}
+
+function cmd.commandtable:EXIT() love.event.quit() end
 
 -- propel: move forward on navigation track at a faster than usual speed, will use more energy
 function cmd.commandtable:PROPEL()
-        
+
+        -- a probability to trigger a minigame
+
+        if math.random() <= 0.1 then
+                -- minigame!
+        end
+
+        cmd.commandtable:CLEAR()
+        local result = (math.random(100) <= cmd.power) and "PROPEL SUCCESS" or "Program received signal SIGSEGV, Segmentation fault"
+        cmd.response =  "*ENGAGING ENGINES*\n"..
+                        "*ACCESSING ENERGY RESERVES*\n"..
+                        "*ENGAGING THRUSTERS*\n"..
+                        "***"..result.."***\n"
+        cmd.power = cmd.power - 10
 end
 -- prospect: seeks possible extractable meteors freefloating to gain and restore some energy, will spend energy but will only get returns probabilistically
 function cmd.commandtable:PROSPECT()
+
+
+        math.random(os.time())
+
+        if math.random() <= 0.1 then
+                -- minigame!
+        end
+
+        cmd.commandtable:CLEAR()
+
+        local result = math.random() > 0.4 and "EXTRACTION SUCCESS" or "NO APPLICABLE ENERGY SOURCE FOUND, DISENGAGING COLLECTED MATERIAL"
+
         -- if minerals are found, then start another "minigame" to catch the resources, use the ship's battery system too.
         -- might also run into storm, have to get out without getting hit
+        cmd.response =  "*ENGAGING LATERAL NET SYSTEM TO CATCH DECOMPOSTABLE DEBRIS*\n"..
+                        "*ANALYZING DEBRIS COMPOSITION*\n"..
+                        "***"..result.."***\n"
+
+        if result == "EXTRACTION SUCCESS" then
+                cmd.power = cmd.power + math.random(8,20)
+        end
 end
 
 function cmd.commandtable:CLEAR()
@@ -73,13 +104,9 @@ function cmd.commandtable:CLEAR()
         cmd.response = ""
 end
 
-function cmd.commandtable:EXIT()
-        love.event.quit()
-end
-
-cmd.power = 100
 
 function cmd.commandtable:HELP()
+        cmd.commandtable:CLEAR()
         cmd.response = "HOPE-SS COMMAND CENTER HELP MANUAL:\n"..
                         "----------------------------------------------\n"..
                         "`CLEAR` to clear terminal window view\n"..
